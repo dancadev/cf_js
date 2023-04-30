a6d0c582203a8824924a4307b3ae35a91cb7c789
dancadev <dancadev@gmail.com>
2023-04-30 17:08:18 -0500

    Terminando condicionales

---
+++ condicionales.js
@@ -0,0 +1,39 @@
+//IF
+console.log('IF: ')
+let n1 = 30 
+
+if(n1 === 30){
+    console.log('El numero es: '+n1)
+}
+
+if(n1 === 20){
+    console.log('El numero es: '+ n1)
+}else{
+    console.log('El numero es: '+ n1)
+}
+
+if(n1<=10){
+    console.log('el numero es igual o mayor a 10 ')
+}else if(n1>=30){
+    console.log('El numero es: '+ n1)
+}else{
+    console.log('El numero no es el indicado')
+}
+
+console.log(' ')
+
+// switch
+console.log('SWITCH: ')
+switch(n1===30){
+    case n1>10:
+        console.log('El numero es 30')
+    break
+    case n1<23:
+        console.log('El numero no es menor a 23')
+    break
+}
+
+
+
+
+

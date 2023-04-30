0f4169a3d217da3f846fce73f6e2be996c67a12d
dancadev <dancadev@gmail.com>
2023-04-30 16:15:48 -0500

    Iniciando operadores

---
+++ operadores.js
@@ -0,0 +1,33 @@
+//aritemeticos
+
+//suma de dos numeros
+let n1 = 23
+let n2 = 45
+let suma = n1+n2
+console.log(suma)
+
+//resta de dos numeros
+let resta = n1-n2
+console.log(resta)
+
+//operacions con parentesis
+let parentesis = (n1+n2) - 12
+console.log(parentesis)
+
+//divisiones
+let division = n1/n2
+console.log(division)
+
+//multiplicaciones
+let multiplicacion = n1*n2
+console.log(multiplicacion)
+
+let x = 1 
+console.log(x)
+let y = ++x
+console.log(y)
+let z = y++ + x
+console.log(x)
+console.log(y)
+console.log(z)
+

5632191b7410b5e46a92fed1d3ac4bd3106dcd21
dancadev <dancadev@gmail.com>
2023-04-30 16:50:27 -0500

    Finalizado operadores.js

--- operadores.js
+++ operadores.js
@@ -4,22 +4,33 @@
 let n1 = 23
 let n2 = 45
 let suma = n1+n2
+console.log('suma de dos numeros: ')
 console.log(suma)
+console.log(' ')
+
 
 //resta de dos numeros
 let resta = n1-n2
+console.log('resta de dos numeros: ')
 console.log(resta)
+console.log(' ')
 
-//operacions con parentesis
+
+//operaciones con parentesis
 let parentesis = (n1+n2) - 12
+console.log('operaciones con parentesis: ')
 console.log(parentesis)
+console.log(' ')
 
 //divisiones
 let division = n1/n2
+console.log('divisiones: ')
 console.log(division)
+console.log(' ')
 
 //multiplicaciones
 let multiplicacion = n1*n2
+console.log('multiplicaciones: ')
 console.log(multiplicacion)
 
 let x = 1 
@@ -30,4 +41,56 @@ let z = y++ + x
 console.log(x)
 console.log(y)
 console.log(z)
+console.log(' ')
+
+//operador typeof : indica el tipo de una variable
+console.log('operador typeof : indica el tipo de una variable: ')
+console.log(typeof(5)) //number
+console.log(typeof('Lunes')) //string
+console.log(typeof(true)) //boolean
+console.log(typeof(undefined)) //undefined
+console.log(' ')
+
+//operadores booleanos negación: convierte el valor booleano en us opuesto
+console.log('operadores booleanos negación: convierte el valor booleano en us opuesto: ')
+console.log(!true) //false
+console.log(!false) //true
+console.log(!!true) //true
+console.log(' ')
+
+//operadores booleanos identidad o igualdad: == igualdad debil === igualdad estricta
+
+console.log('operadores booleanos identidad o igualdad: == igualdad debil === igualdad estricta: ')
+console.log(true === true) //true
+console.log(true === false) //false
+console.log(true !== false) //true
+console.log(true !== true) //false
+console.log(' ')
+
+//operadores de comparación
+console.log('Operadores de comparacion <, >, <=, >=, "a" < "b" :')
+console.log(5>3) //true
+console.log(5<3) //false
+console.log(3>=3) //true
+console.log(2<=1) //false
+console.log("a"<"b") //true
+console.log(' ')
+
+//operadores lógicos
+console.log('Operadores lógicos: ')
+//AND
+
+console.log('AND')
+console.log(true && true) //true
+console.log(true && false) //false
+console.log(false && true) //false
+console.log(false && false) //false
+console.log(' ')
+
+console.log('OR')
+console.log(true || true) //true
+console.log(true || false) //true
+console.log(false || true) //true
+console.log(false || false) //false
+
 

c701062fdb6d34d9f3670d3e37ae5689191bc7ac
dancadev <dancadev@gmail.com>
2023-04-30 15:55:31 -0500

    Iniciando con variables en js

---
+++ variables.js
@@ -0,0 +1,20 @@
+let miDato
+console.log(miDato)
+miDato = 20
+console.log(miDato)
+console.log(typeof(miDato))
+
+let number = 34
+console.log(number)
+console.log(typeof(number))
+
+let si = true
+let no = false
+console.log(si)
+console.log(typeof(si))
+console.log(no)
+console.log(typeof(no))
+
+let dato = "Esto es un string"
+console.log(dato)
+console.log(typeof(dato))

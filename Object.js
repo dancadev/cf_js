83286c6162fc0a0ee6192f4b8a9f40d0befda886
dancadev <dancadev@gmail.com>
2023-05-01 07:55:46 -0500

    Finalizando Objetos

--- Object.js
+++ Object.js
@@ -7,3 +7,12 @@ const monitor = {
 }
 console.log(monitor.marca)
 console.log(monitor.formato)
+monitor.tamaño = "24``"
+console.log(monitor.tamaño)
+console.log(monitor)
+
+//se puede crtear de la siguiente manera:
+
+let objeto = {color:'Rojo'}
+let otroObjeto = new Object({color:'Verde'})
+console.log(otroObjeto.color)

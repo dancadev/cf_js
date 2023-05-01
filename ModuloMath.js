b370432217c93e0ea6255e9fcb0fcedd8a494fa8
dancadev <dancadev@gmail.com>
2023-05-01 10:22:10 -0500

    Modulo Math

---
+++ ModuloMath.js
@@ -0,0 +1,15 @@
+console.log(Math.PI) //numero PI
+console.log(Math.E) //numero E
+console.log(Math.random()) //numero random entre 0 y 1 
+console.log(Math.pow(3,8)) //3 elevado a la 8
+console.log(Math.sqrt(3)) //Raiiz cuadrada de 3 
+console.log(Math.min(25,5,74,8,6,15,23)) //retorna el minimo en en conjunto
+console.log(Math.max(25,5,74,8,6,15,23)) //retorna el maximo en en conjunto
+console.log(Math.floor(8.4)) //retorna la parte entera proxima por debajo en este caso 8
+console.log(Math.ceil(8.4)) //retorna la parte entera proxima por encima en este caso 9
+console.log(Math.round(8.6)) //redondea a la pate entera proxima en este caso 9
+console.log(Math.abs(14.5)) //devuleve el valor absoluto de un numero
+console.log(Math.sin(20)) //Funcion seno
+console.log(Math.cos(20)) //Funcion coseno
+console.log(Math.tan(20)) //Funcion tangente
+console.log(Math.log(20)) //Funcion logaritmo

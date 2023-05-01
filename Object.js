47286099b3b4e4dd0f047a4c6e3168858570e374
dancadev <dancadev@gmail.com>
2023-05-01 07:44:48 -0500

    Iniciando con objetos

---
+++ Object.js
@@ -0,0 +1,9 @@
+const monitor = {
+    marca:"janus",
+    color:"negro",
+    tamaño:23,
+    respuesta:"5ms",
+    formato:[1,2,3,'cuatro']
+}
+console.log(monitor.marca)
+console.log(monitor.formato)
